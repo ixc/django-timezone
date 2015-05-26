6ad909a276b67f31bfcddc617c18560de845bffb
Tai Lee <real.human@mrmachine.net>
2015-05-26 19:25:38 +1000

    Remove more unnecessary settings.

--- timezone/tests/settings.py
+++ timezone/tests/settings.py
@@ -9,25 +9,11 @@ DATABASES = {
     }
 }
 
-DEBUG = True
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.staticfiles',
     'django_nose',
     'timezone.tests'
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
-
-ROOT_URLCONF = 'timezone.tests.urls'
 SECRET_KEY = 'secret-key'
 STATIC_URL = '/static/'
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
